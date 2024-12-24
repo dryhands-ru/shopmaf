dea1ad202dea6b1460e2efa2c0ac8d0486b43a26
Vitamin_IIID <sukhoruchenkovitalik@yandex.ru>
2024-12-24 17:51:52 +0300

    My Django first commit

---
+++ shopmaf_base/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shopmaf_base project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopmaf_base.settings')
+
+application = get_wsgi_application()
